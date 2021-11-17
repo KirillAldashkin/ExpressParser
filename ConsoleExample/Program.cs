de3689349356dbca1914e69a6f726a372ec06130
KirillAldashkin <a89086520092k@yandex.ru>
2021-11-17 23:51:03 +0800

    Added CLI utility

--- ConsoleExample/Program.cs
+++ ConsoleExample/Program.cs
@@ -1,19 +1,155 @@
 ﻿using ExpressParser;
+using static ConsoleExample.Strings;
 
-//just some testing utilite
-//TODO: simple CLI program to create and evaluate expressions
-Expression e = new("(((a+b)*(a+b))+((a+b)*(a+b)))*(((a+b)*(a+b))+((a+b)*(a+b)))");
-e.SetArgument("a", 1.1);
-e.SetArgument("b", 1.1);
-double ddd = 1;
-for(int i = 0; i < 1000000; i++) //avg time on my PC: 3300ms
-{ 
-    double v = e.Evaluate();
-}
-e.Compile(); //avg time on my PC: <50ms
-ddd = 1; 
-for (int i = 0; i < 1000000; i++) //avg time on my PC: 400ms
+Dictionary<string, Expression> expressions = new();
+Console.WriteLine(Greeting);
+while(true)
 {
-    double v = e.EvaluateIL();
+    string command = Console.ReadLine().Trim();
+    if (command == "exit") break;
+    else if (command == "clear") Console.Clear();
+    else if (command == "help") Console.WriteLine(Help);
+    else if (command.StartsWith("compile")) Compile(command);
+    else if (command.StartsWith("create")) Create(command);
+    else if (command.StartsWith("set")) Set(command);
+    else if (command.StartsWith("eval-il")) Eval(command, true);
+    else if (command.StartsWith("eval")) Eval(command, false);
+    else if (command.StartsWith("list")) List(command);
+    else Console.WriteLine(UnknownCommand(command));
 }
-ddd = 1;
+
+string[] GetTokens(string cmd, int start) => cmd[start..]
+    .Trim().Split(' ').Where(s => s.Length > 0).ToArray();
+
+void Create(string cmd)
+{
+    var tokens = GetTokens(cmd, 6);
+    if(tokens.Length != 2)
+        Console.WriteLine(ErrorArgCount(2));
+    else if(expressions.ContainsKey(tokens[0]))
+        Console.WriteLine(ErrorExprExists(tokens[0]));
+    else
+    {
+        Expression expr = null;
+        try
+        {
+            expr = new(tokens[1]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(ErrorCreateExpr(e));
+        }
+        finally
+        {
+            expressions.Add(tokens[0], expr);
+            Console.WriteLine(CreateSuccess(tokens[0]));
+        }
+    }
+}
+
+void Set(string cmd)
+{
+    var tokens = GetTokens(cmd, 3);
+    if (tokens.Length != 3)
+        Console.WriteLine(ErrorArgCount(3));
+    else if (!expressions.ContainsKey(tokens[0]))
+        Console.WriteLine(ErrorExprNotFound(tokens[0]));
+    else if (!expressions[tokens[0]].Arguments.ContainsKey(tokens[1]))
+        Console.WriteLine(ErrorKeyNotFound(tokens[0], tokens[1]));
+    else if(!double.TryParse(tokens[2], out double value))
+        Console.WriteLine(ErrorInvalidDouble(tokens[2]));
+    else
+    {
+        try
+        {
+            expressions[tokens[0]].SetArgument(tokens[1], value);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(ErrorSetVal(e));
+        }
+        finally
+        {
+            Console.WriteLine(SetSuccess(tokens[0], tokens[1], tokens[2]));
+        }
+    }
+}
+
+void Eval(string cmd, bool compiled)
+{
+    var tokens = GetTokens(cmd, compiled ? 7 : 4); //compiled ? "eval-il" : "eval"
+    if (tokens.Length != 1)
+        Console.WriteLine(ErrorArgCount(1));
+    else if (!expressions.ContainsKey(tokens[0]))
+        Console.WriteLine(ErrorExprNotFound(tokens[0]));
+    else
+    {
+        double res = 0;
+        try
+        {
+            res = compiled ? 
+                expressions[tokens[0]].EvaluateIL() : 
+                expressions[tokens[0]].Evaluate();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(ErrorEval(e));
+        }
+        finally
+        {
+            Console.WriteLine(PrintEval(res));
+        }
+    }
+}
+
+void List(string cmd)
+{
+    var tokens = GetTokens(cmd, 4);
+    if (tokens.Length != 1)
+        Console.WriteLine(ErrorArgCount(1));
+    else if (!expressions.ContainsKey(tokens[0]))
+        Console.WriteLine(ErrorExprNotFound(tokens[0]));
+    else
+    {
+        Expression expr = expressions[tokens[0]];
+        if(expr.Arguments.Count==0)
+            Console.WriteLine(ListEmpty(tokens[0]));
+        else
+        {
+            Console.WriteLine(ListStart(tokens[0]));
+            var iter = expr.Arguments.GetEnumerator();
+            iter.MoveNext();
+            bool isLast = false;
+            while (!isLast)
+                Console.WriteLine(ListEntry(
+                    iter.Current.Key, 
+                    iter.Current.Value,
+                    isLast = !iter.MoveNext()
+                ));
+        }
+    }
+}
+
+void Compile(string cmd)
+{
+    var tokens = GetTokens(cmd, 7);
+    if(tokens.Length != 1)
+        Console.WriteLine(ErrorArgCount(1));
+    else if(!expressions.ContainsKey(tokens[0]))
+        Console.WriteLine(ErrorExprNotFound(tokens[0]));
+    else
+    {
+        try
+        {
+            expressions[tokens[0]].Compile();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(ErrorCompileExpr(e));
+        }
+        finally
+        {
+            Console.WriteLine(CompileSuccess(tokens[0]));
+        }
+    }
+}
---
+++ ConsoleExample/Strings.cs
@@ -0,0 +1,75 @@
+﻿namespace ConsoleExample;
+
+// I'm not sure about grammar because I am not England :(
+static class Strings
+{
+    #region Common
+    public static string Greeting = 
+        "Welcome to ExpressParser CLI utility! Type \"help\" to get command list.";
+
+    public static string UnknownCommand(string cmd) =>
+        $"Unknown command: \"{cmd}\". Type 'help' for more info.";
+
+    public static string Help =
+@"> 'exit' - close program
+> 'help' - print this message
+> 'clear' - clears the console
+> 'create <NAME> <EXPRESSION>' - create expression with name <NAME>
+> 'compile <EXPRESSION>' - create IL code for expression <EXPRESSION>
+> 'set <EXPRESSION> <ARGUMENT> <VALUE>' - set argument <ARGUMENT> of expression <EXPRESSION> to <VALUE>
+> 'eval <EXPRESSION>' - evaluates exression and prints result of evaluation
+> 'eval-il <EXPRESSION>' - evaluates compiled code of exression and prints result of evaluation
+> 'list <EXPRESSION>' - prints arguments (and values) of expression";
+    #endregion
+    #region Error while exectuing command
+    public static string ErrorCreateExpr(Exception e) =>
+    $"Error while parsing expression: {e}";
+
+    public static string ErrorCompileExpr(Exception e) =>
+        $"Error while compile expression: {e}";
+
+    public static string ErrorEval(Exception e) =>
+        $"Error while evaluating expression: {e}";
+
+    public static string ErrorSetVal(Exception e) =>
+        $"Error while setting value: {e}";
+    #endregion
+    #region Success/Result
+    public static string CreateSuccess(string name) =>
+    $"Created expression \"{name}\".";
+
+    public static string CompileSuccess(string name) =>
+        $"Compiled expression \"{name}\".";
+
+    public static string SetSuccess(string expr, string arg, string val) =>
+        $"Success: {expr}.{arg}={val}";
+
+    public static string ListEmpty(string expr) =>
+        $"Expression \"{expr}\" contains no arguments.";
+
+    public static string PrintEval(double res) =>
+        $"Evaluation result: {res}";
+
+    public static string ListStart(string expr) =>
+        $"Arguments of expression \"{expr}\":";
+
+    public static string ListEntry(string arg, double val, bool isLast) =>
+        $"{(isLast ? '╙' : '╟')}{arg}\t\t\t= {val}";
+    #endregion
+    #region Invalid command error
+    public static string ErrorArgCount(int c) =>
+        $"Error: {c} arguments excpected. Type \'help\' for more info.";
+
+    public static string ErrorExprExists(string name) =>
+        $"Expression \"{name}\" already exists.";
+
+    public static string ErrorExprNotFound(string name) =>
+        $"Expression \"{name}\" not found.";
+
+    public static string ErrorKeyNotFound(string name, string expr) =>
+        $"Key \"{name}\" not found in expression {expr}.";
+
+    public static string ErrorInvalidDouble(string val) =>
+        $"\"{val}\" is invalid number.";
+    #endregion
+}
